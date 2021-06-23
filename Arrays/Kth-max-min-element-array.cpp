e130dadfc8023b37efa7a65e5cb164a5df8a486e
DeepanshuPratik <deepanshu.pratik@gmail.com>
2021-06-23 19:20:25 +0530

    Kth largest and smallest number in array

---
+++ Arrays/Kth-max-min-element-array.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int arr[], int, int, int);
+int kthLargest(int arr[], int, int, int);
+ 
+int main()
+{  
+    int number_of_elements;
+    cin>>number_of_elements;
+    int a[number_of_elements];
+            
+    for(int i=0;i<number_of_elements;i++)
+        cin>>a[i];
+                
+    int k;
+    cin>>k;
+    cout<<k<<"th smallest element:"<<kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    cout<<k<<"th largest element:"<<kthLargest(a, 0, number_of_elements-1, k)<<endl;
+    
+    return 0;
+}
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int kthSmallest(int arr[], int l, int r, int k) {
+    int max=INT_MIN;
+    for(int i=0;i<r+1;i++){
+        if(arr[i]>max)
+        max=arr[i];
+    }
+    int c=0;
+    int N=1e6+2;
+    bool check[N];
+    for(int i=0;i<N;i++)
+    check[i]=false;
+    
+    for(int i=0;i<=r;i++){
+        check[arr[i]]=true;
+    }
+    int i=0;
+    while(c<k && i<=max){
+        if(check[i]==true)
+        c++;
+        
+        i++;
+    }
+    return i-1;
+}
+
+int kthLargest(int arr[], int l, int r, int k) {
+    int max=INT_MIN;
+    for(int i=0;i<r+1;i++){
+        if(arr[i]>max)
+        max=arr[i];
+    }
+    int c=0;
+    int N=1e6+2;
+    bool check[N];
+    for(int i=0;i<N;i++)
+    check[i]=false;
+    
+    for(int i=0;i<=r;i++){
+        check[arr[i]]=true;
+    }
+    int i=max;
+    while(c<k && i>=0){
+        if(check[i]==true)
+        c++;
+        
+        i--;
+    }
+    return i+1;
+}
