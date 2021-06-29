5205ec949d7ee68f3d09e78f5e5615fe80902c3c
DeepanshuPratik <deepanshu.pratik@gmail.com>
2021-06-29 14:36:03 +0530

    modified finding duplicate with time: O(nlog(n)) and space O(1)

--- Arrays/find-duplicate-element-in-array.cpp
+++ Arrays/find-duplicate-element-in-array.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+// METHOD 2
+void findDuplicate(int v1[], int n) {
+    int p;
+    sort(v1,v1+n);            // sorting takes nlog(n) time complexity
+    for(int i=0;i<n-1;i++){
+        p=v1[i]^v1[i+1];       // p= xor of two adjacent nos and imp: a xor a = 0
+        if(p==0){
+            cout<<endl<<"Duplicate: "<<v1[i];
+            p=v1[i];
+            break;
+        }
+    }
+}
+
 int main(){
     int N,maxs=0;
     cin>>N;
@@ -15,8 +29,13 @@ int main(){
         frequency[arr[i]]++;   // adding frequency of every element in array "arr"
     for(int i=0;i<=maxs;i++){
         if(frequency[i]>1){    // if frequency of element is greater than 1 then print element
-            cout<<i;
+            cout<<"Duplicate: "<<i;
             break;
         }
     }
+    //METHOD 2 with time complexity: O(nlog(n)) and space complexity: O(1)
+    findDuplicate(arr,N);
+    return 0;
 }
+
+
