f8898a739c38b1cab8e4555598f3debad1113226
DeepanshuPratik <deepanshu.pratik@gmail.com>
2021-07-05 23:22:32 +0530

    corrected with all nos instead of positive nos

--- Arrays/move-zeroes-without-changing-order.cpp
+++ Arrays/move-zeroes-without-changing-order.cpp
@@ -10,21 +10,21 @@ int main(){
         cin>>arr[i];
     int start=0,end=1;
     while(end!=n){
-        if(arr[start]==0 && arr[end]>0){
+        if(arr[start]==0 && arr[end]!=0){
             int temp=arr[start];
             arr[start]=arr[end];
             arr[end]=temp;
             start++;
             end++;
         }
-        else if(arr[start]>0 && arr[end]>0 && start==(end-1)){
+        else if(arr[start]!=0 && arr[end]!=0 && start==(end-1)){
             if(start<n-2){
                 start=end+1;
                 end=start+1;
             }
             else {start++; end++;}
         }
-        else if(arr[start]>0 && arr[end]>0){       // eg n=5 0 1 0 2 0 where s=start e=end
+        else if(arr[start]!=0 && arr[end]!=0){       // eg n=5 0 1 0 2 0 where s=start e=end
             start++;                               //  0 1 0 2 0
         }                                          //  s e
         else end++;                                //  1 0 0 2 0
