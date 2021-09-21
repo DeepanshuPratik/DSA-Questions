baa0181f0e68e01e47915465739cc05e17549856
DeepanshuPratik <deepanshu.pratik@gmail.com>
2021-09-22 01:12:58 +0530

    Added example diagram

--- Stacks/LargeAreaHistogram.cpp
+++ Stacks/LargeAreaHistogram.cpp
@@ -1,32 +1,31 @@
-/* 
-
-
- 
-*/
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-#define fast ios_base::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
-#define FOR(n) for(int i=0;i<n;i++)
-#define endl cout<<endl
-#define read(b,n);  for(int i=0;i<n;i++) cin>>b[i];
-#define print(b,n);  for(int i=0;i<n;i++) cout<<b[i]<<" ";
-#define pll pair<ll,ll>
-#define pcl pair<char,ll>
-#define pb push_back
-#define vl vector<ll>
-#define popb pop_back
-#define all(v) (v).begin(), (v).end()
-#define fir first
-#define sec second
-#define size(x) (int)(x.size())
-ll i,j;
+/*
+INPUT:
+           8                                                                           
+           __                           
+     6    |  | 6                        
+     __ 5 |  |__                        
+    |  |__|  |  |2                          
+    |  |  |  |  |__                         
+    |__|__|__|__|__|                      
 
-void solve(ll a[], ll n){
 
-    ll left[n],right[n];
+PROCESSED 1:
+
+           8
+           __
+     6    |  | 6
+     __ 5 |  |__
+    |..|..|..|..|2         dotted portion => rectangle area --> 5*4 as ((height)*(no of unit distant inputs))           
+    |. |  |  | .|__
+    |..|..|..|..|__|     
+*/
+int largestRectangleArea(vector<int>& a) {
+    int n=a.size();
+    int left[n],right[n];
     stack<int> st;
-    // left filling
+    // left index filling of next smaller element
     left[0]=-1;
     st.push(0);
     for(int i=1;i<n;i++){
@@ -40,7 +39,7 @@ void solve(ll a[], ll n){
         }
         st.push(i);
     }
-    // right filling
+    // right index filling of next smaller element
     stack<int> h;
     right[n-1]=n;
     h.push(n-1);
@@ -55,27 +54,11 @@ void solve(ll a[], ll n){
         }
         h.push(i);
     }
-    // answer
-    ll ans=INT_MIN;
-    FOR(n){
-        ll temp= (right[i]-left[i] - 1)*a[i];
+    // calculating area of corresponding data arranged.
+    int ans=INT_MIN;
+    for(int i=0;i<n;i++){
+        int temp= (right[i]-left[i] - 1)*a[i];
         ans=max(temp,ans);
     }
-    cout<<ans;
-    
+    return(ans);
 }
-    
-int main()
-{
-    fast;
-    int t;
-    cin>>t;
-    ll a[t];
-    read(a,t);
-    solve(a,t);
-    
-}
-
-
-
-
